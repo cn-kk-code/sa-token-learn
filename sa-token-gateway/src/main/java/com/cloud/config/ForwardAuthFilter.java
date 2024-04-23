df3eacf3adb6a1c29064083383638733a5a66045
aven <397013586@qq.com>
2024-04-23 09:12:40 +0800

    新增配置

---
+++ sa-token-gateway/src/main/java/com/cloud/config/ForwardAuthFilter.java
@@ -0,0 +1,35 @@
+package com.cloud.config;
+
+/**
+ * description ForwardAuthFilter <br>
+ *
+ * @author 文学武 <br>
+ * @version 1.0 <br>
+ * @date 2024/4/22 18:05 <br>
+ */
+
+import cn.dev33.satoken.same.SaSameUtil;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 全局过滤器，为请求添加 Same-Token
+ */
+@Component
+public class ForwardAuthFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest newRequest = exchange
+                .getRequest()
+                .mutate()
+                .header(SaSameUtil.SAME_TOKEN, SaSameUtil.getToken())
+                .build();
+        ServerWebExchange newExchange = exchange.mutate().request(newRequest).build();
+        return chain.filter(newExchange);
+    }
+}
+
